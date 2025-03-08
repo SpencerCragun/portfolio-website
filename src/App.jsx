3fabbbc2e5451253eed501983b131d441df857ec
Spencer Cragun <Spencer.cragun@gmail.com>
2025-03-07 16:20:05 -0800

    added early pages and components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import './App.css'
+import React from 'react'
+import Home from './pages/Home'
+import Navbar from './components/Navbar'
 
 function App() {
 
   return (
     <>
+      <Navbar />
+      <Home />
     </>
   )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <img alt='spencer cragun'></img>
+          <li>About</li>
+          <li>Projects</li>
+          <li>Contact</li>  
+        </ul>
+      </nav>
+    </div>
+  )
+}
